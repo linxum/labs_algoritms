1524a98b3a9f3cc52f18674678a80307daa920b1
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2022-12-19 17:07:38 +0300

    Update Lab 3.cpp

--- Lab 3/Lab 3.cpp
+++ Lab 3/Lab 3.cpp
@@ -4,16 +4,10 @@
 #include <string>
 
 using namespace std;
+int const N = 40;
+int dist[N][N]; int used[N][N];
 
-int dist[1001][1001];
-int n, m;
-char a[1001][1001];
-int used[1001][1001];
-queue <pair <int, int> > q;
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
-
-int CanGo(int i, int j) {
+int CanGo(int i, int j, int n) {
 	if ((i < 0) || (i >= n) || (j < 0) || (j >= n)) return 0;
 	if (dist[i][j] != -1) {
 		return 0;
@@ -22,7 +16,10 @@ int CanGo(int i, int j) {
 		return 1;
 	}
 }
-void bfs(int sx, int sy, int ex, int ey) {
+void bfs(int sx, int sy, int ex, int ey, int n) {
+	int dx[4] = { 1, -1, 0, 0 };
+	int dy[4] = { 0, 0, 1, -1 };
+	queue <pair <int, int> > q;
 	q.push(make_pair(sx, sy));
 	used[sx][sy] = 1;
 	dist[sx][sy] = 0;
@@ -36,7 +33,7 @@ void bfs(int sx, int sy, int ex, int ey) {
 		for (int i = 0; i < 4; i++) {
 			int x1 = x + dx[i];
 			int y1 = y + dy[i];
-			if (CanGo(x1, y1) && !used[x1][y1]) {
+			if (CanGo(x1, y1, n) && !used[x1][y1]) {
 				q.push(make_pair(x1, y1));
 				dist[x1][y1] = dist[x][y] + 1;
 				used[x1][y1] = 1;
@@ -46,7 +43,8 @@ void bfs(int sx, int sy, int ex, int ey) {
 }
 int main()
 {
-	int startx, starty, endx, endy;
+	char a[N][N];
+	int startx, starty, endx, endy, n;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
@@ -66,7 +64,7 @@ int main()
 		}
 	}
 
-	bfs(startx, starty, endx, endy);
+	bfs(startx, starty, endx, endy, n);
 
 	if (dist[endx][endy] == -1) {
 		cout << "N" << endl;
@@ -93,7 +91,7 @@ int main()
 				a[x][y] = '+';
 			}
 		}
-		a[endx][endy] = '+';
+		a[x][y] = '+';
 		cout << "Y" << endl;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
