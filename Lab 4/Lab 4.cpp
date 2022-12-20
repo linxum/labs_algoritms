10075e0b64d1927cde091eab4f698db6f36d376b
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2022-12-20 17:42:44 +0300

    Create Lab 4.cpp

---
+++ Lab 4/Lab 4.cpp
@@ -0,0 +1,28 @@
+#define INF 2000000000
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+	int n, k, val;
+	int sum[100000];
+	cin >> n;
+	cin >> k;
+	sum[0] = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> val;
+		sum[i] = sum[i - 1] + val;
+	}
+
+	int min = INF; int max = -INF;
+	for (int i = k; i <= n; i++)
+	{
+		if (sum[i] - sum[i - k] < min)
+			min = sum[i] - sum[i - k];
+		if (sum[i] - sum[i - k] > max)
+			max = sum[i] - sum[i - k];
+	}
+	cout << min / k << endl << max / k;
+}
